b6205af89564d75b0e6b0480e434ed53282f06ce
kaushikrishi <rishi@loyalytics.in>
2021-07-17 18:32:47 +0530

    basic route

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/", require("./routes"));
 // require("./models");
-// TODO: Database connection	promisify
+// TODO: Database connection promisify
+app.get("/", (req, res) => {
+    return res.send("built for bon voyage 🛳️");
+});
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`🚀 Server running localhost://${PORT}`);
