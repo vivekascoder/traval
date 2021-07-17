7778e532840dfdc4c1b246f76b17d3c7dba59b56
kaushikrishi <rishi@loyalytics.in>
2021-07-17 18:31:00 +0530

    setup: workflow

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/", require("./routes"));
-require("./models");
+// require("./models");
 // TODO: Database connection	promisify
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
